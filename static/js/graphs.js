7a41d8fca7293ad6f375516e4eda255c311820d0
sol_rosser <solomonj2k12@hotmail.co.uk>
2018-08-02 14:07:36 +0000

    creating my first bar chart which shows the number of cars towed and their make

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,7 +1,29 @@
 queue()
-    .defer(d3.csv, "data/Happy.csv")
-    .await(MakeGraphs);
+    .defer(d3.csv, "data/towed-vehicles.csv")
+    .await(makeGraphs);
     
-function MakeGraphs(error, happyData){
+function makeGraphs(error, towedData){
+    var ndx = crossfilter(towedData);
     
+    show_towed_make(ndx);
+    
+    dc.renderAll();
+}
+
+function show_towed_make(ndx){
+    var dim = ndx.dimension(dc.pluck('Make'));
+    var group = dim.group();
+    
+    dc.barChart("#make-balance")
+        .width(600)
+        .height(300)
+        .margins({top:10, right:50, bottom:30, left:50})
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Car Make")
+        .yAxis().ticks(10);
 }
